d1ee47b85bdd36a11a82a0e70d9627a13e0911c2
mingxiongyan <1091231756@qq.com>
2018-01-31 21:39:25 +0800

    by mingxiong

--- src/components/mingxiong/Home.vue
+++ src/components/mingxiong/Home.vue
@@ -1,12 +1,14 @@
 <template>
     <div class="Home">
        <slide-show></slide-show>
-       <!-- <pic-list></pic-list> -->
+       <pic-list></pic-list>
+       <slide-picture></slide-picture>
     </div>
 </template>
 <script>
 import slideShow from './slideShow'
 import picList from './picList'
+import slidePicture from './slidePicture'
 
 export default {
     name: "Home",
@@ -17,13 +19,15 @@ export default {
     },
     components: {
         slideShow,
-        picList
+        picList,
+        slidePicture
     }
 }
 </script>
 <style lang="css" scoped>
     .Home{
-        width: 31.25rem;
+        width: 10rem;
+        height: 366.373333rem;
         overflow: hidden;
     }
 </style>
---
+++ src/components/mingxiong/slidePicture.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="slidePicture">
+        <figure v-for="value in imgList">
+            <img src="https://img07.jiuxian.com/2018/0125/238888580d9b438eaa2806e2a94b2b4a4.jpg" alt="">
+        </figure>
+    </div>
+</template>
+<script>
+export default {
+    name: "slidePictrue",
+    data () {
+        return {
+            imgList: [1,2,3,4]
+        };
+    }
+}
+</script>
+<style lang="css" scoped>
+    .slidePicture{
+        width: 10rem;
+        background-color: red;
+        display: flex;
+        justify-content: space-between;
+    }
+    .slidePicture figure{
+        width: 2.8rem;
+        height: 4.533333rem;
+        background-color: pink;
+    }
+    .slidePicture figure img {
+        width: 2.666667rem;
+        height: 2.533333rem;
+    }
+</style>
