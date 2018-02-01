0457af4944a0a9a96bd0d4f342e35cfcb756ed25
linzengjian <284080213@qq.com>
2018-02-01 11:32:23 +0800

    link now

---
+++ src/components/zengjian/ceng.vue
@@ -0,0 +1,24 @@
+<template>
+    
+</template>
+
+
+<script>
+export default {
+    name:"ceng.vue",
+    data () {
+        return{
+
+        }
+    },
+    //方法写这里
+    methods: {
+
+    }
+}
+</script>
+
+
+<style>
+    
+</style>
--- src/components/zengjian/mine.vue
+++ src/components/zengjian/mine.vue
@@ -1,14 +1,28 @@
 <template>
-    <p>我的</p>
+     <div>
+         <!-- 头部 -->
+        <Bar-Sort-Top :msg='msg'></Bar-Sort-Top>
+    </div>
 </template>
 
 <script>
+import BarSortTop from '../yaowu/barSort/barSortTop/BarSortTop.vue'
+import mine from './mine.vue'
     export default {
-    
+        name: "mine",
+        data () {
+            return {
+                msg:'用户登录'
+            };
+        },
+        components: {
+            BarSortTop
+        }
     }
 </script>
 
 <style scoped>
-
+       .nav_top{height: 40px;background: red;}
+       .nav_top a span{margin-left:140px;line-height: 40px;}
 </style>
 
