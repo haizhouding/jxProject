1120a5b19fe6fc0ed669cfc9aa87bce11f6fb9a8
linzengjian <284080213@qq.com>
2018-02-01 11:56:33 +0800

    haizhou

--- src/router/index.js
+++ src/router/index.js
@@ -5,22 +5,15 @@ Vue.use(Router)
 import mine from '../components/zengjian/mine'
 import home from '../components/mingxiong/Home.vue'
 import types from '../components/yaowu/types.vue'
-<<<<<<< HEAD
-
-import cart from '../components/zengjian/cart.vue'
 //跳到商品列表
 import list from '../components/yaowu/BarList.vue'
 
 import cart from '../components/haizhou/cart.vue'
 
-=======
-import cart from '../components/haizhou/cart.vue'
-
-import list from '../components/yaowu/list.vue'
 
 
 
->>>>>>> c1141c5ecf14bb168c02c885da6158c9e132e241
+
 
 
 export default new Router({
