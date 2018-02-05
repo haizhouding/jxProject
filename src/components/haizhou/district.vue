6ee259098dfb45cfb0ec19f21ab34cfc651538ee
haizhouding <291727105@qq.com>
2018-02-05 09:13:46 +0800

    haizhou

--- src/components/haizhou/district.vue
+++ src/components/haizhou/district.vue
@@ -110,7 +110,6 @@ export default {
     }
    
      #district .regions p:first-of-type b{
-         display: inline-block;
          height: .266667rem;
          width: .266667rem;
          /* background: red; */
@@ -144,9 +143,11 @@ export default {
     #district .region {
         position: fixed;
         top: 0;
+        left: 0;
         width: 100%;
         height: 100%;
-        background: rgba(0, 0, 0, 0.3)
+        background: rgba(0, 0, 0, 0.3);
+        z-index: 9999;
     }
 
     </style>
